6939059572198c750629918e82bb1f98ef18bdcd
monyet <d4rk_1@yahoo.com>
2013-09-02 13:58:27 +0700

    Update test.php

--- public/test.php
+++ public/test.php
@@ -1,1 +1,7 @@
-<?php echo 'This is a test of pushies.'; ?>
+
+<?php
+    $version = phpversion();
+    $ip = $_SERVER["SERVER_ADDR"];
+    $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+    $temp = sys_get_temp_dir();
+?> 
